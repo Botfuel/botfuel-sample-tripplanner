6a32134acce801f6260a3eb585d7e688e97a40f8
Canh <canh85@gmail.com>
2018-04-13 16:16:21 +0200

    refactor: update sdk v5.0.0

--- src/dialogs/trip-dialog.js
+++ src/dialogs/trip-dialog.js
@@ -7,16 +7,17 @@ Trip.params = {
   entities: {
     destination: {
       dim: 'city',
+      priority: 10,
+    },
+    tripType: {
+      dim: 'trip-type',
+      priority: 5,
     },
     date: {
       dim: 'time',
     },
     passengerNumber: {
       dim: 'number',
-    },
-    tripType: {
-      dim: 'trip-type',
-      priority: 10,
     },
   },
 };
