a006cc1af49defb793aa70cacedd6f24e7e3646a
daniellepintz <daniellepintz@gmail.com>
2018-04-20 10:38:42 +0200

    chore: updated implementation of render

--- src/views/trip-view.js
+++ src/views/trip-view.js
@@ -33,7 +33,7 @@ const askInfo = (entityName) => {
 };
 
 class TripView extends PromptView {
-  renderEntities(matchedEntities, missingEntities) {
+  render(userMessage, {matchedEntities, missingEntities}) {
     const messages = [];
 
     // Print info of obtained information
